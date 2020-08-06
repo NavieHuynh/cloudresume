83194f1e62f21f0cc4e1241525af92c23b48e79e
Navie Huynh <huynhnavie@gmail.com>
2020-08-05 20:57:38 -0500

    added cloud resume github link and sample javascript code

---
+++ script.js
@@ -0,0 +1,8 @@
+console.log("Cookies: " + navigator.cookieEnabled);
+console.log("Browser Language: " + navigator.browserLanguage);
+console.log("Language: " + navigator.language);
+console.log("Platform: " + navigator.platform);
+console.log("Connection Speed: " + navigator.connectionSpeed);
+console.log("User Agent: " + navigator.userAgent);
+console.log("Webdriver: " + navigator.webdriver);
+console.log("Geolocation: " + navigator.geolocation.getCurrentPosition());
